387bfeb5ef619595b57997cc1761e0ad062a83e2
gelsonjr <gelsonjr@gmail.com>
2022-10-27 14:02:20 -0400

    added a basic landing page

---
+++ client/src/components/Main.js
@@ -0,0 +1,16 @@
+const Main = () => { 
+  return (
+    <div className="main">
+      <div className="slogan">
+        <h1>Skyrocket Your</h1>
+        <h1 className="alt-color">Organic Traffic</h1>
+        <p>Manage and grow your portfolio from anywhere with ease</p>
+        <div className="get-started">
+          <a href="#">Get Started &gt;</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
