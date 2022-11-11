148e29be0d7b2a321ce22306149a7cd6f5826529
Santiago Aday <74830912+santiaday@users.noreply.github.com>
2022-11-10 22:54:34 -0500

    Add files via upload

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,13 +1,21 @@
+ 
+
 const Main = () => { 
   return (
     <div className="main">
       <div className="slogan">
         <h1>Skyrocket Your</h1>
         <h1 className="alt-color">Organic Traffic</h1>
-        <p>Manage and grow your portfolio from anywhere with ease</p>
-        <div className="get-started">
-          <a href="#">Get Started &gt;</a>
-        </div>
+        <p>Boost your search engine optimization with ease.</p>
+        <div class="c-formContainer">
+  <form class="c-form" action="">
+    <input class="c-form__input" placeholder="E-mail" type="email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" required="" />
+    <label class="c-form__buttonLabel" for="checkbox">
+      <button class="c-form__button" type="button">Send</button>
+    </label>
+    <label class="c-form__toggle" for="checkbox" data-title="Notify me"></label>
+  </form>
+</div>
       </div>
     </div>
   );
