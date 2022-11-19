3c80b7fa0ab66eb80c2c0243c989591dae75862e
gelsonjr <gelsonjr@gmail.com>
2022-11-19 13:26:02 -0500

    made teams and footer responsive

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,16 +4,18 @@ import SearchBar from "../SearchBar/SearchBar";
 
 const Footer = () => {
   return (
-    <div className="footer-container">
-      <h1>Try it out now!</h1>
-      <h2>All it takes is one click to begin your SEO journey</h2>
-      <h3>
-        Simply type in the term that you would like to rank for and <br />
-        Crawler.io will do the rest for you.
-      </h3>
-      <div className="footer-search-button">
-        <div className="footer-search-button-wrapper">
-          <SearchBar />
+    <div className="footer-outer-container">
+      <div className="footer-container">
+        <h1>Try it out now!</h1>
+        <h2>All it takes is one click to begin your SEO journey</h2>
+        <h3>
+          Simply type in the term that you would like to rank for and <br />
+          Crawler.io will do the rest for you.
+        </h3>
+        <div className="footer-search-button">
+          <div className="footer-search-button-wrapper">
+            <SearchBar />
+          </div>
         </div>
       </div>
     </div>
